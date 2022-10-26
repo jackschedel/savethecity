d494fa7d5ca8465f3c0014f7410d1c18582971a6
Jack Schedel <jackschedel@gmail.com>
2022-10-26 11:27:23 -0400

    refactored dashboard as playscreen
    Dashboard = Playscreen

--- client/app/src/main/java/com/citysavers/savethecity/ui/playscreen/PlayscreenFragment.java
+++ client/app/src/main/java/com/citysavers/savethecity/ui/playscreen/PlayscreenFragment.java
@@ -1,4 +1,4 @@
-package com.citysavers.savethecity.ui.dashboard;
+package com.citysavers.savethecity.ui.playscreen;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.citysavers.savethecity.databinding.FragmentDashboardBinding;
+import com.citysavers.savethecity.databinding.FragmentPlayscreenBinding;
 
-public class DashboardFragment extends Fragment {
+public class PlayscreenFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentPlayscreenBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        PlayscreenViewModel playscreenViewModel =
+                new ViewModelProvider(this).get(PlayscreenViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentPlayscreenBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textPlayscreen;
+        playscreenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- client/app/src/main/java/com/citysavers/savethecity/ui/playscreen/PlayscreenViewModel.java
+++ client/app/src/main/java/com/citysavers/savethecity/ui/playscreen/PlayscreenViewModel.java
@@ -1,16 +1,16 @@
-package com.citysavers.savethecity.ui.dashboard;
+package com.citysavers.savethecity.ui.playscreen;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class PlayscreenViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public PlayscreenViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is playscreen fragment");
     }
 
     public LiveData<String> getText() {
